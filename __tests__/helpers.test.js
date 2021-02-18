e0fdee2d66e610c8390b43e25723911a323ddfbb
Russ <ocjade@comcast.net>
2021-02-18 10:57:11 -0700

    quick fix

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,28 +1,4 @@
-const { format_date } = require('../utils/helpers');
-
-test('format_date() returns a date string', () => {
-    const date = new Date('2020-03-20 16:12:03');
-
-    expect(format_date(date)).toBe('3/20/2020')
-})
-
-const { format_plural } = require('../utils/helpers');
-
-test('format_plural() returns the plural version of word', () => {
-    const word = 'Tiger';
-    const ammount = 2;
-
-    expect(format_plural(word, ammount)).toBe('Tigers');
-})
-
-test('format_plural() returns the single version of word', () => {
-    const word = 'Lion';
-    const ammount = 1;
-
-    expect(format_plural(word, ammount)).toBe('Lion');
-})
-
-const { format_url } = require('../utils/helpers');
+const { format_date, format_plural, format_url } = require('../utils/helpers');
 
 test('format_url() returns a simplified url string', () => {
     const url1 = format_url('http://test.com/page/1');
@@ -32,4 +8,18 @@ test('format_url() returns a simplified url string', () => {
     expect(url1).toBe('test.com');
     expect(url2).toBe('coolstuff.com');
     expect(url3).toBe('google.com');
+});
+
+test('format_plural() returns a pluralized word', () => {
+    const word1 = format_plural('tiger', 1);
+    const word2 = format_plural('lion', 2);
+
+    expect(word1).toBe('tiger');
+    expect(word2).toBe('lions');
+});
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+
+    expect(format_date(date)).toBe('3/20/2020');
 });
